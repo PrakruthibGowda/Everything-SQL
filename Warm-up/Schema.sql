d67c4c6d9dafa31a2c02cee65efc2f8e1d4ec831
Prakruthi Gowda <77579923+PrakruthibGowda@users.noreply.github.com>
2026-03-02 22:23:33 +0000

    Create Schema.sql
    File to store all schemas for sql questions in this folder

---
+++ Warm-up/Schema.sql
@@ -0,0 +1,11 @@
+--EMPLOYEE
+CREATE TABLE Employee (
+    employee_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100),
+    hire_date DATE,
+    salary DECIMAL(10,2),
+    department_id INT
+);
+
